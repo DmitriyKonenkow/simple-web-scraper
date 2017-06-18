0bf25813fa31f965ae927d9ff1314704980e315e
Dmitriy Konenkow <d.konenkow@gmail.com>
2017-06-18 05:55:15 +0300

    added config

--- src/main/java/ru/spb/konenkow/App.java
+++ src/main/java/ru/spb/konenkow/App.java
@@ -1,13 +1,35 @@
 package ru.spb.konenkow;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+    private static final Properties properties;
+
+    public static Properties getProperties() {
+        return properties;
+    }
+
+
+    static {
+        properties = new Properties();
+        try (FileReader fileReader = new FileReader("scraper.conf")) {
+            properties.load(fileReader);
+        } catch (IOException e) {
+            LOG.error("Config not found.", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
